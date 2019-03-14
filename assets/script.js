7b8e617241f0d6fe1e83f38db507257bc9edfd23
ann-leonard <aleo0779@gmail.com>
2019-03-14 13:36:02 -0400

    updated display

--- assets/script.js
+++ assets/script.js
@@ -9,20 +9,33 @@ var topics = [
 function makeButtons() {
   for (i = 0; i < topics.length; i++) {
     var btn = $("<button>");
-    // Adds a class of movie to our button
-    btn.addClass("topic btn btn-outline-info");
-    // Added a data-attribute
+    btn.addClass("topic btn btn-info mr-3 mb-5");
     btn.attr("data-topic", topics[i]);
-    // Provided the initial button text
     btn.text(topics[i]);
-    // Added the button to the buttons-view div
     $("#buttons-view").append(btn);
   }
 }
 makeButtons();
 
+$(document).keyup(function(event) {
+  if (event.key == "Enter") {
+      submit()
+  }
+})
+$("#submitBtn").on("click", submit())
+
+function submit(){
+  if ($("#topicInput").val() != ''){
+    var newTopic = $("#topicInput").val().trim()
+    topics.push(newTopic)
+    $("#buttons-view").empty()
+    makeButtons()
+    $("#topicInput").val('')
+  }
+}
+
 var queryURL='';
-$(".topic").on("click", function() {
+$(document.body).on("click", ".topic", function() {
   $(".gifContainer").empty()
   var searchTerm = $(this).attr("data-topic").replace(" ","+")
   queryURL =
@@ -39,36 +52,39 @@ $(".topic").on("click", function() {
   }).then(function(response) {
     
    var gif_list=[];
-   console.log(response)
    for (i = 0; i<10;i++){
-      var gif = $(
-           `<img class="gif mt-5 ml-3" data-state= "still" src=${
-             response.data[i].images.fixed_height_still.url
-           }>`
-         )
+     
+      var rating = response.data[i].rating
+      var domRating= `<span class="badge badge-pill badge-info">${rating}</span>`
       var gifStill = response.data[i].images.fixed_height_still.url
       var gifAnimate = response.data[i].images.fixed_height.url
+      var gif = $(
+        `<img class="gif mb-4 ml-4 mr-4" data-state= "still" src=${
+          response.data[i].images.fixed_height_still.url
+        }>`
+        + domRating 
+      )
       gif.attr("data-still",gifStill)
       gif.attr("data-animate",gifAnimate)
            gif_list.push(gif)
            $(".gifContainer").append(gif)
-           
-     
-  
-     
-    }  
-          $(".gif").on("click",function(){
-            var state = $(this).attr("data-state");
-            if(state === "still"){
-              var animatedSrc = $(this).attr("data-animate")
-            $(this).attr("src", animatedSrc)
-            $(this).attr("data-state","animate")
+          $(".gifContainer").append(gif)
+    } 
+
+    $(".").on("click",function(){
+    var state = $(this).attr("data-state");
+      if(state === "still"){
+        var animatedSrc = $(this).attr("data-animate")
+        $(this).attr("src", animatedSrc)
+        console.log(animatedSrc)
+        console.log($(this).attr("data-animate"))
+        $(this).attr("data-state","animate")
           }
-          else{
-            var stillSrc = $(this).attr("data-still")
-            $(this).attr("src", stillSrc)
-            $(this).attr("data-state","still")
-          }
-          })  
+      else{
+        var stillSrc = $(this).attr("data-still")
+        $(this).attr("src", stillSrc)
+        $(this).attr("data-state","still")
+       }
+    })  
 
 })}
